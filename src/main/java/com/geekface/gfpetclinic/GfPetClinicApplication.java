b33c8ca37a303474718ef200fee0af8b382c79df
zubermohammad@gmail.com <zubermohammad@gmail.com>
2020-06-15 22:18:32 +0300

    Initial Commit

---
+++ src/main/java/com/geekface/gfpetclinic/GfPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.geekface.gfpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GfPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GfPetClinicApplication.class, args);
+	}
+
+}
