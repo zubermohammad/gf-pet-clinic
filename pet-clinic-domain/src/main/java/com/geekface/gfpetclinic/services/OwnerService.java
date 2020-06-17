8359eef1e01e2c9d8bbeb6573f6f8a0394d04b0c
zubermohammad@gmail.com <zubermohammad@gmail.com>
2020-06-17 10:33:45 +0300

    refactor(domain): refactor to common crud service interface
    Closes: #5

--- pet-clinic-domain/src/main/java/com/geekface/gfpetclinic/services/OwnerService.java
+++ pet-clinic-domain/src/main/java/com/geekface/gfpetclinic/services/OwnerService.java
@@ -2,15 +2,7 @@ package com.geekface.gfpetclinic.services;
 
 import com.geekface.gfpetclinic.domain.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 }
--- pet-clinic-domain/src/main/java/com/geekface/gfpetclinic/services/VetService.java
+++ pet-clinic-domain/src/main/java/com/geekface/gfpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package com.geekface.gfpetclinic.services;
 
 import com.geekface.gfpetclinic.domain.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
