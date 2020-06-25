d2bfe73db92613b74b4061887a91570ffd12e0af
zubermohammad@gmail.com <zubermohammad@gmail.com>
2020-06-26 00:36:45 +0300

    feat(UI): list owner & vet from bootstraped data
    Closes: #13

--- pet-clinic-web/src/main/java/com/geekface/gfpetclinic/controllers/owner/OwnerController.java
+++ pet-clinic-web/src/main/java/com/geekface/gfpetclinic/controllers/owner/OwnerController.java
@@ -1,13 +1,22 @@
 package com.geekface.gfpetclinic.controllers.owner;
 
+import com.geekface.gfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
--- pet-clinic-web/src/main/java/com/geekface/gfpetclinic/controllers/vets/VetsController.java
+++ pet-clinic-web/src/main/java/com/geekface/gfpetclinic/controllers/vets/VetsController.java
@@ -1,14 +1,23 @@
 package com.geekface.gfpetclinic.controllers.vets;
 
+import com.geekface.gfpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/vets")
 public class VetsController {
 
+    private final VetService vetService;
+
+    public VetsController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"", "/"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
